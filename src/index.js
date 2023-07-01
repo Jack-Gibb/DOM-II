959594d20ee5e229514fab7bb20a9a700ea168a9
Jack Gibb <ogrewithissues@yahoo.com>
2023-07-01 10:50:14 -0600

    events done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 import './less/index.less'
 
 // Your code goes here!
+
+window.onload = function (evt) {
+    const heading = document.querySelector('h1');
+    heading.textContent = 'FUNvee';
+
+
+window.addEventListener('copy', () => {
+    navigator.clipboard.readText()
+    .then(text => {
+        heading.textContent += text;
+    })
+})
+
+document.body.addEventListener('click', evt => {
+    evt.target.classList.toggle('mirror')
+})
+
+document.body.addEventListener('dblclick', evt => {
+    heading.textContent = 'Party Bus'
+})
+
+window.addEventListener('keydown', evt => {
+    if (evt.key == 6) {
+        document.body.innerHTML = '<h1>Third of the Beast</h1>'
+}
+})
+
+document.body.addEventListener('mousemove', evt => {
+    const { clientX, clientY } = evt
+})
+
+const destinations = document.querySelectorAll('.destination')
+for (let destination of destinations) {
+    destination.addEventListener('mouseenter', () => {
+        destination.style.fontWeight = 'bold'
+    })
+    destination.addEventListener('mouseleave', () => {
+        setTimeout(() => {
+            destination.style.fontWeight = 'initial'
+            
+        }, 500);
+    })
+}
+}
